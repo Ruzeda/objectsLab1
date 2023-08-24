04d0697d256a7e2f47c5bb1932182e8416cd099b
Ruzeda Fields <fields.rozay@gmail.com>
2023-08-23 22:49:41 -0700

    complete

---
+++ index.js
@@ -0,0 +1,87 @@
+carThree = [
+    wheels: '4',
+    owner: 'Ruzeda',
+    isBroken: false,
+    color: 'pink'
+];
+
+carThree.price = 80000;
+console.log(carThree);
+
+carThree = ['year'] = 2023;
+
+carThree.isBroken = true;
+
+carThree.price
+
+clientIsInterested = ;
+
+if (carThree.color === 'blue') { 
+    clientIsInterested = true;
+    console.log('They want to purchase the car!'); } else {
+        clientIsInterested = false;
+        console.log('We did not make the sale');
+};
+
+store = [
+    profit: 8000,
+    manager: 'David',
+    isOpenNow: true,
+    isVeryExspensive: false
+];
+
+store.location = 'Aruba';
+store['storeType'] = 'Fashion Boutique';
+store.isOpenNow = false;
+store.isVeryExspensive;
+let canShopHere = ;
+if (store.isOpenNow == true && store.isVeryExspensive == false) {
+    store.canShopHere =  true;
+    console.log('we cannot shop here!'); } else {
+        store.canShopHere = false;
+        console.log('we can shop here!');
+    };
+
+delete store.profit;
+
+home = [
+    rooms: 5,
+    bathrooms: 3,
+    isForSale: false,
+    isInGoodLocation: true
+];
+
+home.price = 800000;
+home['year'] = 2009;
+home.isForSale = true;
+home.bathrooms;
+let buyHouse = ;
+
+if (home.isForSale == true && home.isInGoodLocation == true) {
+    home.buyHouse = true; } else {
+        home.buyHouse = false;
+};
+
+delete home.isForSale;
+
+ruzedaFields = [
+    Model: true,
+    Student: true,
+    Age: 26,
+    Sex: 'Female'
+];
+
+ruzedaFields.placeOfBirth = 'Tanzania';
+ruzedaFields['homeTown'] = 'Seattle';
+ruzedaFields.Student = false;
+ruzedaFields.Model;
+let isSoftwareEngineer = ;
+
+if (ruzedaFields.Student == true && ruzedaFields.Model == true) {
+    isSoftwareEngineer = true; } else {
+        isSoftwareEngineer = false;
+}
+
+
+
+delete.Sex;
